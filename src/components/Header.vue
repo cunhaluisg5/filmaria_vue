5dc5761f9cf199b4712bef9912daaf9c6510e4f8
Luís Cipriani <cunhaluisg5@gmail.com>
2024-02-21 20:09:21 -0300

    Cria componente Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <header id="header">
-    <span>Filmaria</span>
+    <router-link to='/' exact>Filmaria</router-link>
+    <router-link to='/salvos' exact class="meusFilmes"><button>SALVOS</button></router-link>
   </header>
 </template>
 
@@ -11,5 +12,29 @@ export default {
 </script>
 
 <style scoped>
+  #header{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    height: 60px;
+    background-color: brown;
+  }
 
+  #header a{
+    text-decoration: none;
+    font-size: 30px;
+    color: #FFF;
+    cursor: pointer;
+  }
+
+  button{
+    cursor: pointer;
+    border: 0;
+    background: #FFF;
+    padding: 5px 15px;
+    outline: none;
+    border-radius: 5px;
+    font-weight: bold;
+  }
 </style>
